1fa30c1eef5dbc61e1e2b817ca77d65399409048
Karene85 <70654346+Karene85@users.noreply.github.com>
2021-02-27 15:16:28 +0530

    Updated ip address -2
    Updated ip address -2

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -10,7 +10,7 @@ class Controller extends Component {
   constructor()
   {
     super();
-    this.baseUrl = "http://54.209.238.198:8080/api/v1/";
+    this.baseUrl = "http://34.207.119.115:8080/api/v1/";
   }
   render(){
     return(
