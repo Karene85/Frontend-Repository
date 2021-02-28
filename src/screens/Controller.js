4387e7f4733ea39e45468b8f99440102bfb60aa1
Karene85 <70654346+Karene85@users.noreply.github.com>
2021-02-28 12:41:15 +0530

    IP address update - 3
    IP address updated third time.

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -10,7 +10,7 @@ class Controller extends Component {
   constructor()
   {
     super();
-    this.baseUrl = "http://34.207.119.115:8080/api/v1/";
+    this.baseUrl = "http://54.226.26.56:8080/api/v1/";
   }
   render(){
     return(
